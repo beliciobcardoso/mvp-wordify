8a4d0ae9429e995afc0e41a1e40cd7ff90adea31
Belicio Batista Cardoso <belloinfo@gmail.com>
2025-01-02 11:57:51 -0300

    configure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  }
+};
 
 export default nextConfig;
